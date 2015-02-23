0c4fb5fb5f9cccd4d89f607efe52cda4260f7990
nfl <nfl@odoo.com>
2015-02-23 21:00:54 +0100

    [DEV] recup info JDC

--- newz/add_newz.php
+++ newz/add_newz.php
@@ -2,19 +2,25 @@
 <?php $control = "admin" ; include("access_control.php") ?>
 
 <htlm> <head>
-    <title> Newz - Home </title>
+    <title> Newz - Adder </title>
     <link href="add_newz.css" rel="stylesheet">
     <link href="index.css" rel="stylesheet">
+
+    <script>
+        function input_del(){
+            document.getElementById("input").innerHTML = "" ;
+        }
+    </script>
 </head>
 
 <body>
 <?php include("index.php") ?>
 
     <div id="main">
-        To add newz, copy the url in the textarea below.
+        To add newz, copy the url in the textarea below.<br/>
         Make sure you have one link per line (separtor : LF).
         <form action="newz_create.php" method="POST">
-            <textarea cols="50" row="10" name="urls">
+            <textarea cols="50" row="10" name="urls" id="input" class="input" onclick="input_del()">
                 Urls goes here !
             </textarea><br/>
             <input type="submit" value="SEND"/>
--- newz/newz_create.php
+++ newz/newz_create.php
@@ -1,12 +1,39 @@
+<meta charset="UTF-8">
 <?php include("account.php") ; ?>
 
 <?php
+function jdg($xpath, $page){
+    $res = [] ;
+    $res["title"] = $xpath->query("//div[@class='post-title']//h2")->item(0)->textContent ;
+    $res["author"] = $xpath->query("//div[@class='post-title']//p/a")->item(0)->textContent ;
+    $res["content"] = substr(explode("</div>", explode("post-content", $page)[1])[0], 2) ;
+    return $res ;
+}
+
 if( $USER_ID && isset($_POST["urls"]) ){
     $urls = explode("\n", trim($_POST["urls"]) ) ;
     include("connect_db.php") ;
     foreach( $urls as $url ){
-        $query = "INSERT INTO newz(url, create_uid, dt_creation) VALUES('$url', $USER_ID, NOW()) ;" ;
-        mysqli_query($link, $query) or die("ERROR SQL : $query") ;
+        unset($result) ;
+        $page = file_get_contents($url) ;
+        $doc = new DOMDocument ;
+        $doc->loadHTMLFile($url) ;
+        $xpath = new DOMXPath($doc) ;
+
+        if(preg_match("/journaldugeek/", $url)){
+            $result = jdg($xpath, $page) ;
+        } else {
+             ;
+        }
+        // echo "ICI $result" ;
+        if(isset($result)){
+            echo $tilte = $result["title"] ;
+            echo $author = $result["author"] ;
+            echo $content = $result["content"] ;
+            $query = "INSERT INTO newz(url, title, author, content, create_uid, dt_creation) VALUES('$url', '$title', '$author', '$content', $USER_ID, NOW()) ;" ;
+            mysqli_query($link, $query) or die("ERROR SQL : $query") ;
+        }
+
     }
 }
-header( "Location: home.php" ) ;
+// header( "Location: home.php" ) ;
